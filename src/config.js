3b14a00cb860161b4afce3c69936868ff47749c8
Adam Kirkwood <hello@adamkirkwood.com>
2016-12-22 14:04:21 -0800

    :snowflake: Prevent exception when process.execPath is undefined

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  __PRODUCTION__: process && (process.execPath.search('electron-prebuilt') === -1),
-  __DEVELOPMENT__: process && (process.execPath.search('electron-prebuilt') !== -1),
+  __PRODUCTION__: (typeof process !== "undefined") && (typeof process.execPath !== "undefined") && (process.execPath.search('electron-prebuilt') === -1) || false,
+  __DEVELOPMENT__: (typeof process !== "undefined") && (typeof process.execPath !== "undefined") && (process.execPath.search('electron-prebuilt') !== -1) || true,
   __ELECTRON__: (typeof process !== "undefined") && process.versions && (process.versions.electron !== undefined),
 
   RELEASE_SERVER_URL: 'https://sketchpacks-releases.herokuapp.com',

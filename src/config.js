bbe70bd9f82d7fc8ea4587bbd4aa500ea0ae8aca
Adam Kirkwood <hello@adamkirkwood.com>
2016-12-22 13:39:03 -0800

    :snowflake: Add __ELECTRON__ flag to config file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   __PRODUCTION__: process && (process.execPath.search('electron-prebuilt') === -1),
   __DEVELOPMENT__: process && (process.execPath.search('electron-prebuilt') !== -1),
-  __ELECTRON__: process && (process.execPath.search('electron-prebuilt') === -1),
+  __ELECTRON__: (typeof process !== "undefined") && process.versions && (process.versions.electron !== undefined),
 
   RELEASE_SERVER_URL: 'https://sketchpacks-releases.herokuapp.com',
   APP_VERSION: require('../package.json').version,

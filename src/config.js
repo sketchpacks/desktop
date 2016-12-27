5f9f9ca51b29ad711804a09bfe5811504861d60e
Adam Kirkwood <hello@adamkirkwood.com>
2016-12-27 15:34:01 -0800

    :sparkles: User can view PluginDetails

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ module.exports = {
   APP_VERSION: require('../package.json').version,
   SERVER_PORT: process.env.PORT || 8080,
 
-  WEB_URL: process.env.NODE_ENV === 'development' ? 'https://sketchpacks-dev.firebaseapp.com' : 'https://www.sketchpacks.com',
+  WEB_URL: 'https://sketchpacks-dev.firebaseapp.com',
   API_URL: 'https://sketchpacks-api.herokuapp.com/',
 
   CATALOG_FETCH_DELAY: '10s',

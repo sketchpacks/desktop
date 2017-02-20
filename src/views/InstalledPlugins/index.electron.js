d3a25fe96a9eaab9ce485e171e7a79ffbfe1cc07
Adam Kirkwood <hello@adamkirkwood.com>
2017-02-20 08:21:02 -0800

    :lipstick: Empty States

--- src/views/InstalledPlugins/index.electron.js
+++ src/views/InstalledPlugins/index.electron.js
@@ -12,7 +12,12 @@ class InstalledPluginsContainer extends Component {
 
     if (plugins.isLoading) return (<div>Loading plugins...</div>)
 
-    if (plugins.length === 0) return (<div>No plugins found</div>)
+    if (plugins.length === 0) return (
+      <div className="empty-state--expanded">
+        <h4>No installed plugins found</h4>
+        <p>Your installed plugins will be shown here</p>
+      </div>
+    )
 
     return (<PluginList plugins={plugins} />)
   }
@@ -22,16 +27,6 @@ class InstalledPluginsContainer extends Component {
 
     return (
       <div>
-        <div className="container">
-          <div className="row">
-            <div className="column">
-              <h3 className="page-title">
-                Your plugins
-              </h3>
-            </div>
-          </div>
-        </div>
-
         { this.renderList() }
       </div>
     )

9d51ded9988f494171ef39dcc48615b006052fa9
Adam Kirkwood <hello@adamkirkwood.com>
2017-02-19 23:49:57 -0800

    :bug: Open remote URLs in default browser

--- src/components/Nameplate/index.js
+++ src/components/Nameplate/index.js
@@ -1,13 +1,19 @@
+import {
+  __ELECTRON__,
+  WEB_URL,
+  API_URL
+} from 'config'
+
 import React from 'react'
 
 import { Link } from 'react-router'
 
 import Avatar from 'components/Avatar'
 
-const Nameplate = ({ handle, thumbnailUrl, name }) => (
-  <div>
-    <Link to={`/@${handle}`}>
-      <div className="o-media">
+const Nameplate = ({ handle, thumbnailUrl, name }) => __ELECTRON__
+  ? (
+    <div>
+      <div className="o-media" onClick={() => require('electron').remote.shell.openExternal(`${WEB_URL}/@${handle}`)}>
         <div className="o-media__left u-mar-right-x-small">
           <Avatar handle={handle} />
         </div>
@@ -16,8 +22,22 @@ const Nameplate = ({ handle, thumbnailUrl, name }) => (
           {handle || name}
         </div>
       </div>
-    </Link>
-  </div>
-)
+    </div>
+  )
+  : (
+    <div>
+      <Link to={`/@${handle}`}>
+        <div className="o-media">
+          <div className="o-media__left u-mar-right-x-small">
+            <Avatar handle={handle} />
+          </div>
+
+          <div className="o-media__content">
+            {handle || name}
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
 
 export default Nameplate

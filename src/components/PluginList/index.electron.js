74ec27af6a5cde50c0aaf6a8098c41f77566703f
Adam Kirkwood <hello@adamkirkwood.com>
2017-02-26 10:29:29 -0800

    :lipstick: Simpler PluginMedia

--- src/components/PluginList/index.electron.js
+++ src/components/PluginList/index.electron.js
@@ -2,20 +2,16 @@ import React from 'react'
 
 import PluginMedia from 'components/PluginMedia'
 
+import './styles.scss'
+
 const PluginList = ({ plugins, authorDetails }) => {
   return (
-    <div className="container container--padded-top">
+    <div className="o-plugin-list">
       {plugins.items.map((plugin, idx) => {
         // Set the owner property if not present
         if (!('owner' in plugin)) plugin['owner'] = authorDetails
 
-        return (
-          <div className="row" key={`${idx}-${plugin.id}`}>
-            <div className="column">
-              <PluginMedia plugin={plugin} />
-            </div>
-          </div>
-        )
+        return <PluginMedia plugin={plugin} key={`${idx}-${plugin.id}`} />
       })}
     </div>
   )

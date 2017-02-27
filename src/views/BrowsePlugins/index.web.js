d99c9bd0bfc4547b7f852e87193a8ce58048318c
Adam Kirkwood <hello@adamkirkwood.com>
2017-02-27 07:21:01 -0800

    :art: List Sorter method

--- src/views/BrowsePlugins/index.web.js
+++ src/views/BrowsePlugins/index.web.js
@@ -18,6 +18,7 @@ class BrowsePluginsContainer extends Component {
     super(props)
 
     this.fetchData = this.fetchData.bind(this)
+    this.renderListSorter = this.renderListSorter.bind(this)
   }
 
   componentDidMount () {
@@ -68,6 +69,34 @@ class BrowsePluginsContainer extends Component {
     )
   }
 
+  renderListSorter () {
+    return (
+      <div className="button-group">
+        <button type="button" className="button-clear button--dropdown" onClick={() => {}}>
+          <span>Sort:</span>
+          <span>{plugins.sort.split(':')[0]}</span>
+        </button>
+        <ul className={"dropdown__menu is-visible"}>
+          <li onClick={() => dispatch(catalogSortBy('score:desc'))}>
+            <span>Most Popular</span>
+          </li>
+          <li onClick={() => dispatch(catalogSortBy('created_at:desc'))}>
+            <span>Newest</span>
+          </li>
+          <li onClick={() => dispatch(catalogSortBy('updated_at:desc'))}>
+            <span>Recently Updated</span>
+          </li>
+          <li onClick={() => dispatch(catalogSortBy('title:asc'))}>
+            <span>Name</span>
+          </li>
+          <li onClick={() => dispatch(catalogSortBy('compatible_version:desc'))}>
+            <span>Supported Sketch Version</span>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
   render () {
     const {plugins,dispatch} = this.props
 
@@ -76,31 +105,6 @@ class BrowsePluginsContainer extends Component {
         <div className="container">
           <div className="row">
             <div className="column column__content">
-
-              <div className="button-group">
-                <button type="button" className="button-clear button--dropdown" onClick={() => {}}>
-                  <span>Sort:</span>
-                  <span>{plugins.sort.split(':')[0]}</span>
-                </button>
-                <ul className={"dropdown__menu is-visible"}>
-                  <li onClick={() => dispatch(catalogSortBy('score:desc'))}>
-                    <span>Most Popular</span>
-                  </li>
-                  <li onClick={() => dispatch(catalogSortBy('created_at:desc'))}>
-                    <span>Newest</span>
-                  </li>
-                  <li onClick={() => dispatch(catalogSortBy('updated_at:desc'))}>
-                    <span>Recently Updated</span>
-                  </li>
-                  <li onClick={() => dispatch(catalogSortBy('title:asc'))}>
-                    <span>Name</span>
-                  </li>
-                  <li onClick={() => dispatch(catalogSortBy('compatible_version:desc'))}>
-                    <span>Supported Sketch Version</span>
-                  </li>
-                </ul>
-              </div>
-
               <PluginList
                 plugins={plugins}
               />

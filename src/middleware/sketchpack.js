4c471001772e51b3e6b816472ca16fd9c26505b7
Adam Kirkwood <hello@adamkirkwood.com>
2017-03-18 23:23:47 -0700

    :horse_racing: Only record changes to the library

--- src/middleware/sketchpack.js
+++ src/middleware/sketchpack.js
@@ -4,17 +4,20 @@ const jsonfile = require('jsonfile')
 const libraryPath = `${os.homedir()}/Desktop/sketchpack.json`
 
 const sketchpackMiddleware = store => next => action => {
+  const prevState = store.getState().library.items
   next(action)
+  const nextState = store.getState().library.items
 
-  const contents = {
-    plugins: store.getState().library.items
+  if (prevState !== nextState) {
+    const contents = {
+      plugins: store.getState().library.items
+    }
+
+    jsonfile.writeFile(libraryPath, contents, {spaces: 2}, (err) => {
+      if (err) console.error(err)
+      console.log(contents)
+    })
   }
-
-  jsonfile.writeFile(libraryPath, contents, {spaces: 2}, (err) => {
-    if (err) console.error(err)
-    console.log(contents)
-  })
-
 }
 
 export default sketchpackMiddleware

03f92041725ef4d5c13fd8d7806dc1cb129b90c4
Adam Kirkwood <hello@adamkirkwood.com>
2017-03-19 23:17:32 -0700

    :ok_hand: Better sanitization of SemVer

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,13 +5,19 @@ const sanitizeSemVer = (semver) => {
 	if (semver === 0) return "0.0.0"
 	if (semver === "0") return "0.0.0"
 
+  const sanitize = (point) => point.toString().replace(/[^0-9]/g, '')
+
   const x = semver.split('.')
 
-  const major = parseInt(x[0]) || 0
-  const minor = parseInt(x[1]) || 0
-  const patch = parseInt(x[2]) || 0
+  const major = x[0] || 0
+  const minor = x[1] || 0
+  const patch = x[2] || 0
 
-  return [major,minor,patch].join('.')
+  return [
+			sanitize(major),
+			sanitize(minor),
+			sanitize(patch)
+		].join('.')
 }
 
 module.exports = {
